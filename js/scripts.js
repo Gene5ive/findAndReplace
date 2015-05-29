981b937b5a35d45b03a69543712bd8caa0563c7e
Frank <jedigene@gmail.com>
2015-05-29 14:43:41 -0700

    completed front-end integration

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,18 @@ var findAndReplace = function(sentence, find, replace) {
   sentence = sentence.join(" ");
   return sentence;
 };
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var sentence = ($("input#sentence").val());
+    var find = ($("input#find").val());
+    var replace = ($("input#replace").val());
+
+    var newSentence = findAndReplace(sentence, find, replace);
+
+    $("#return").show();
+    $('#newSentence').text(newSentence);
+
+    event.preventDefault();
+  });
+});
